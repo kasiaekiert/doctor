e3521782b2e5bed23a8291f1c8439d9e66c08912
Kasia <kasia.ekiert@gmail.com>
2020-04-21 19:33:26 +0200

    navbar into sideheader

--- client/bundles/WebStore/components/SiteHeader.jsx
+++ client/bundles/WebStore/components/SiteHeader.jsx
@@ -8,6 +8,52 @@ export class SiteHeader extends React.Component {
           <a href='/' className='navbar-brand d-flex align-items-center'>
             <strong>Web Store</strong>
           </a>
+          <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+            <ul className='navbar-nav'>
+              <li className='nav-item active'>
+                <a className='nav-link' href='#'>
+                  Home <span className='sr-only'>(current)</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='#'>
+                  Features
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='/products'>
+                  SHOP
+                </a>
+              </li>
+              <li className='nav-item dropdown'>
+                <a
+                  className='nav-link dropdown-toggle'
+                  href='#'
+                  id='navbarDropdownMenuLink'
+                  role='button'
+                  data-toggle='dropdown'
+                  aria-haspopup='true'
+                  aria-expanded='false'
+                >
+                  Dropdown link
+                </a>
+                <div
+                  className='dropdown-menu'
+                  aria-labelledby='navbarDropdownMenuLink'
+                >
+                  <a className='dropdown-item' href='#'>
+                    Action
+                  </a>
+                  <a className='dropdown-item' href='#'>
+                    Another action
+                  </a>
+                  <a className='dropdown-item' href='#'>
+                    Something else here
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
           <span className='navbar-brand'>
             In Cart: {this.props.shopping_cart_item_count}
           </span>
--- client/packs/web-store-bundle.js
+++ client/packs/web-store-bundle.js
@@ -1,6 +1,5 @@
 import ReactOnRails from "react-on-rails";
 
-import { Navbar } from "../bundles/WebStore/components/Navbar.jsx";
 import { ProductCard } from "../bundles/WebStore/components/ProductCard.jsx";
 import { ProductRow } from "../bundles/WebStore/components/ProductRow.jsx";
 import { SiteHeader } from "../bundles/WebStore/components/SiteHeader.jsx";
@@ -8,7 +7,6 @@ import { ShoppingCartList } from "../bundles/WebStore/components/ShoppingCartLis
 import { CheckoutForm } from "../bundles/WebStore/components/CheckoutForm.jsx";
 
 ReactOnRails.register({
-  Navbar,
   SiteHeader,
   ProductCard,
   ProductRow,
